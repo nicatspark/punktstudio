c4df5e4d6dc2bcee55038bb96936889ad072e0a7
Nicolas Hervy <nicolas@hervy.se>
2023-02-08 14:56:53 +0100

    Extracted footer. Meta tags. Added feature article.

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+export const SITE_TITLE = 'My personal website.'
+export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_KEYWORDS =
+  'interior design, interior decoration, sofia, bulgaria'
+export const INTRO1 = `We are an interior decorator company that provides the highest quality
+services to clients looking to elevate their space. Our team of
+experienced designers and decorators work with you to create a
+personalized look that reflects your style and budget.`
+export const INTRO2 = `We offer a variety of services, from simple color changes to complete
+room renovations, to help you create the perfect space for your needs.
+With our help, you can turn any room into a beautiful, inviting space.`
