a405f26e8ee112d86622b71810a00687414e8cd9
Nicolas Hervy <nicolas@hervy.se>
2023-02-08 16:43:00 +0100

    About page.

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,4 @@
-export const SITE_TITLE = 'My personal website.'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_TITLE = 'Welcome to Punkt Studio - Interior Design company'
 export const SITE_KEYWORDS =
   'interior design, interior decoration, sofia, bulgaria'
 export const INTRO1 = `We are an interior decorator company that provides the highest quality
@@ -9,3 +8,4 @@ personalized look that reflects your style and budget.`
 export const INTRO2 = `We offer a variety of services, from simple color changes to complete
 room renovations, to help you create the perfect space for your needs.
 With our help, you can turn any room into a beautiful, inviting space.`
+export const SITE_DESCRIPTION = INTRO1
