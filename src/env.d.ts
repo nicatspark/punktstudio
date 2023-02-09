df552f7241d48ca3e9b065113e6a6fee4cc24dcb
Nicolas Hervy <nicolas@hervy.se>
2023-02-09 11:06:52 +0100

    Add .env file.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_GMAPSKEY: string
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
