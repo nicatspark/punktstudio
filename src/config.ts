a11203ea5376b0946d9e84a5d50e99042c1b5235
Nicolas Hervy <nicolas@hervy.se>
2023-02-11 22:57:43 +0100

    Change of hero image and some text.

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,6 @@
 export const SITE_TITLE = 'Welcome to Studiopunkt - Interior Design company'
 export const SITE_KEYWORDS =
   'interior design, interior decoration, sofia, bulgaria'
-export const INTRO1 = `We are an interior decorator company that provides the highest quality
-services to clients looking to elevate their space. Our team of
-experienced designers and decorators work with you to create a
-personalized look that reflects your style and budget.`
-export const INTRO2 = `We offer a variety of services, from simple color changes to complete
-room renovations, to help you create the perfect space for your needs.
-With our help, you can turn any room into a beautiful, inviting space.`
+export const INTRO1 = `We are an interior design studio that provides the highest quality services to clients looking to elevate their space. Our team of experienced designers and decorators work with you to create a personalized look that reflects your style and fits your budget.`
+export const INTRO2 = `We offer interior design, interior space layout and decoration while maintaining the ongoing communication process with our clients and all other parties involved. Our approach is always different and based on the context. We aim to go beyond just a good-looking vision, and we create interiors which influence the lifestyle, the emotions, and the senses. We can turn your desired interior into reality.`
 export const SITE_DESCRIPTION = INTRO1
