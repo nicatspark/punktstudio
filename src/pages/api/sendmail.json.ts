66a440897bf61fe18ffadcc10b9ef34cf1f4b739
Nicolas Hervy <nicolas@hervy.se>
2023-02-23 10:40:35 +0100

    Trying with host instead.

--- src/pages/api/sendmail.json.ts
+++ src/pages/api/sendmail.json.ts
@@ -14,11 +14,15 @@ export const post: APIRoute = async ({ request }) => {
     const email = formData.email
     const tel = formData.tel
     const subject = formData.subject
-    const message = formData.message
+    const message = `${formData.message}
+    ----------------------------------------------------------------------
+    From: ${name} ${surname} • email: ${email} • tel: ${tel}
+    `
+    const html = `<div style="margin: 20px auto;font-family: Helvetica, Verdana, sans-serif">${message}</div>`
 
     // sendmail
     let mailTransporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.gmail.com',
       port: 587,
       secure: false,
       auth: {
@@ -31,27 +35,29 @@ export const post: APIRoute = async ({ request }) => {
       from: email,
       to: emailTo,
       subject: `${new URL(request.url).hostname}: ${subject}`,
-      text: `${message}
-----------------------------------------------------------------------
-From: ${name} ${surname} • email: ${email} • tel: ${tel}
-`,
+      text: message,
+      html,
     }
+
     console.log(
       '***************************** mailDetails',
       mailDetails,
       import.meta.env.EMAIL
     )
-    mailTransporter.sendMail(mailDetails, (error, info) => {
+
+    let mailresult = mailTransporter.sendMail(mailDetails, (error, info) => {
       if (error) {
-        return console.log(error)
+        return console.log('******* Error: ', error)
       }
       console.log('Message sent: %s', info.messageId)
     })
+
+    console.log('mailresult', mailresult)
 
     // return response
     return new Response(JSON.stringify(mailDetails), {
       status: 200,
     })
   }
-  return new Response(null, { status: 400 })
+  return new Response(null, { status: 400 }) // if not a json request
 }
